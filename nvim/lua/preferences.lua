3901fbb6e08e801462c7bb3217f527edccbdae7c
felipe <fpassada@ibm.com>
2022-02-25 01:07:39 -0300

    feat: migrate init.vim to init.lua - add option to relativenumbers - add option to list chars - add complete option

--- nvim/lua/preferences.lua
+++ nvim/lua/preferences.lua
@@ -1,9 +1,12 @@
 local opt = vim.opt
 
+opt.completeopt = "menuone,noinsert,noselect"
 opt.expandtab = true
 opt.shiftwidth = 2
 opt.softtabstop = 2
 opt.number = true
+opt.relativenumber = true
+opt.list = true
 opt.hlsearch = false
 opt.smartindent = true
 opt.splitbelow = true
