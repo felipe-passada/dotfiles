b68efb6116fac84268c6dd3204f5eb05952a1853
felipe <fpassada@ibm.com>
2022-02-25 01:09:47 -0300

    feat: migrate keymapings from init.vim to lua file - add map helper function to easly set keymappings - add keymappings for window navigation and telescope

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,19 @@
+-- map helper function
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend("force", options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+vim.g.mapleader = ' '
+
+-- Window navigation
+map('n', '<leader>j', '<C-w>j')
+map('n', '<leader>k', '<C-w>k')
+map('n', '<leader>h', '<C-w>h')
+map('n', '<leader>l', '<C-w>l')
+-- Telescope
+map('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<cr>]], { silent=true })
+map('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], { silent = true })
+map('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').buffers()<cr>]], { silent = true })
+map('n', '<leader>fh', [[<cmd>lua require('telescope.builtin').help_tags()<cr>]], { silent = true })
