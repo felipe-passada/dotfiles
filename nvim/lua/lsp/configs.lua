b1a095aa91583dc608c8b94a650901714cde5b7b
felipe <fpassada@ibm.com>
2022-06-11 19:41:52 -0300

    ref: change require path, throwing error on import

--- nvim/lua/lsp/configs.lua
+++ nvim/lua/lsp/configs.lua
@@ -12,11 +12,11 @@ lsp_installer.setup {
 
 for _, server in pairs(servers) do
   local opts = {
-    on_attach = require("lua.lsp.handlers").on_attach,
-    capabilities = require("lua.lsp.handlers").capabilities,
+    on_attach = require("lsp.handlers").on_attach,
+    capabilities = require("lsp.handlers").capabilities,
   }
 
-  local has_custom_opts, server_custom_opts = pcall(require, "lua.lsp.settings.".. server)
+  local has_custom_opts, server_custom_opts = pcall(require, "lsp.settings.".. server)
 
   if has_custom_opts then
     opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
