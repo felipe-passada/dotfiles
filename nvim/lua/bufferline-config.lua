a6bbc5b0e47d1cc98c7f19c45693aa37f204756b
felipe <fpassada@ibm.com>
2022-09-26 20:00:40 -0300

    fix(bufferline-config): error when opening neovim, due to obsolete config in bufferline config

--- nvim/lua/bufferline-config.lua
+++ nvim/lua/bufferline-config.lua
@@ -11,7 +11,10 @@ bufferline.setup {
     right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
     left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
     middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
-    indicator_icon = '▎',
+    indicator = {
+      style = 'icon',
+      icon = '▎',
+    }, 
     buffer_close_icon = '',
     modified_icon = '●',
     close_icon = '',
@@ -30,7 +33,7 @@ bufferline.setup {
     show_close_icon = true,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-    separator_style = "thick",
+    separator_style = "thin",
     enforce_regular_tabs = true,
     always_show_bufferline = true,
   },
