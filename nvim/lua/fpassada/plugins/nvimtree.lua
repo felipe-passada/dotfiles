1e4757918824a318ea9c22510962e874527a4fe1
felipe <felipepassada@gmail.com>
2023-11-19 23:00:43 -0300

    refactor(tree): add custom icons
    add commented section regarding nvimtree navigation and split on open
    buffer because is it not working as expected will leave comment for now
    with the `fix` key for the future

--- nvim/lua/fpassada/plugins/nvimtree.lua
+++ nvim/lua/fpassada/plugins/nvimtree.lua
@@ -11,7 +11,6 @@ return {
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
-
     nvimtree.setup({
       disable_netrw = true,
       hijack_netrw = true,
@@ -64,8 +63,62 @@ return {
           },
         },
       },
+      renderer = {
+        icons = {
+          glyphs = {
+            folder = {
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+            },
+            git = {
+              unstaged = "",
+              staged = "S",
+              unmerged = "",
+              renamed = "➜",
+              deleted = "",
+              untracked = "U",
+              ignored = "◌",
+            }
+          }
+        }
+      }
 
     })
+    -- FIX: edit_or_open and vslpit not working properly needs maybe a debug and understand why its remaping vim navigation
+    --[[ local api = require("nvim-tree.api")
+
+    local function edit_or_open()
+      local node = api.tree.get_node_under_cursor()
+
+      if node.nodes ~= nil then
+        -- expand or collapse folder
+        api.node.open.edit()
+      else
+        -- open file
+        api.node.open.edit()
+        -- Close the tree if file was opened
+        api.tree.close()
+      end
+    end
+
+    -- open as vsplit on current node
+    local function vsplit_preview()
+      local node = api.tree.get_node_under_cursor()
+
+      if node.nodes ~= nil then
+        -- expand or collapse folder
+        api.node.open.edit()
+      else
+        -- open file as vsplit
+        api.node.open.vertical()
+      end
+
+      -- Finally refocus on tree if it was lost
+      api.tree.focus()
+    end ]]
 
     local keymap = vim.keymap
 
@@ -73,5 +126,11 @@ return {
     keymap.set('n', '<leader>t',':NvimTreeToggle<cr>', { desc = "Toggle file explorer" })
     keymap.set('n', '<leader>r',':NvimTreeRefresh<cr>', { desc = "Refresh file explorer" })
     keymap.set('n', '<leader>f',':NvimTreeFindFile<cr>', { desc = "Toggle file explorer on current file" })
+    -- on_attach
+    -- FIX: not working properly navigation inside buffer stop working when i set to nvim-tree
+    -- keymap.set('n', 'l', edit_or_open, opts)
+    -- keymap.set('n', 'L', vsplit_preview, opts)
+    -- keymap.set('n', 'h', api.node.navigate.parent_close, opts)
+    -- keymap.set('n', 'H', api.tree.collapse_all, opts)
   end
 }
