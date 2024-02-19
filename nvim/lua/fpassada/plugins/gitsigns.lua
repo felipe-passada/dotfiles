e5ef2107b90733af3e31d667e3d811337a28b8ec
felipe <felipepassada@gmail.com>
2024-02-19 15:53:42 -0300

    refactor: make git config simplier

--- nvim/lua/fpassada/plugins/gitsigns.lua
+++ nvim/lua/fpassada/plugins/gitsigns.lua
@@ -51,47 +51,10 @@ return {
       },
     }
 
-    on_attach = function(bufnr)
-      local gs = package.loaded.gitsigns
+    local keymap = vim.keymap
 
-      local function map(mode, l, r, opts)
-        opts = opts or {}
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
-      end
-
-      -- Navigation
-      map('n', ']c', function()
-        if vim.wo.diff then return ']c' end
-        vim.schedule(function() gs.next_hunk() end)
-        return '<Ignore>'
-      end, {expr=true})
-
-      map('n', '[c', function()
-        if vim.wo.diff then return '[c' end
-        vim.schedule(function() gs.prev_hunk() end)
-        return '<Ignore>'
-      end, {expr=true})
-
-      -- Actions
-      map('n', '<leader>hs', gs.stage_hunk, "Stage hunks")
-      map('n', '<leader>hr', gs.reset_hunk, "Reset hunks")
-      map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, "Stage hunks visual mode")
-      map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, "Reset hunks visual mode")
-      map('n', '<leader>hS', gs.stage_buffer, "Stage buffer")
-      map('n', '<leader>hu', gs.undo_stage_hunk, "Undo stage hunk")
-      map('n', '<leader>hR', gs.reset_buffer, "Reset buffer")
-      map('n', '<leader>hp', gs.preview_hunk, "Preview hunk")
-      map('n', '<leader>hb', function() gs.blame_line{full=true} end, "Toggle git blame")
-      map('n', '<leader>tb', gs.toggle_current_line_blame,  "Toggle git blame current line")
-      map('n', '<leader>hd', gs.diffthis, "Toggle git diff")
-      map('n', '<leader>hD', function() gs.diffthis('~') end, "Toggle git diff")
-      map('n', '<leader>td', gs.toggle_deleted, "Toggle git delete")
-
-      -- Text object
-      map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-    end
-
+    keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {desc = 'Git preview hunk'})
+    keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', {desc = 'Git current line blame'})
 
   end
 }
